7f0d7050c665bc6f40ee704336fca986cd9757ad
jayrellona088 <jayrellona@gmail.com>
2022-06-10 15:23:32 +0800

    JSeffect

--- js/script.js
+++ js/script.js
@@ -6,6 +6,12 @@ $(document).ready(function(){
     });
 
     $(window).scroll(function(){
+    	if(this.scrollY > 20) {
+    		$('.navbar').addClass("sticky");
+    	}else{
+    		$('.navbar').removeClass("sticky");
+    	}
+
     	if(this.scrollY > 500){
     		$('.scroll-up-btn').addClass("show");
     	} else{
