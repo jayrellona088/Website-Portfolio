5710cc0fa3984622aa757d03089d662282cc64cc
jayrellona088 <jayrellona@gmail.com>
2022-06-10 14:28:39 +0800

    TestCommit

--- js/script.js
+++ js/script.js
@@ -5,15 +5,28 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active");
     });
 
+    $(window).scroll(function(){
+    	if(this.scrollY > 500){
+    		$('.scroll-up-btn').addClass("show");
+    	} else{
+    		$('.scroll-up-btn').removeClass("show");
+    	}
+    });
+
+    //slide-up script
+    $('.scroll-up-btn').click(function(){
+    	$('html').animate({scrollTop:0});
+    });
+
     // typing text animation script
 	var typed = new Typed(".typing", {
-		strings: ["Programmer", "Developer", "Student" , "Gammer"],
+		strings: ["Programmer", "Developer", "Student" , "Gamer"],
 		typeSpeed: 100,
 		backSpeed: 60,
 		loop:true
 	});
 	var typed = new Typed(".typing2", {
-		strings: ["Programmer", "Developer", "Student" , "Gammer"],
+		strings: ["Programmer", "Developer", "Student" , "Gamer"],
 		typeSpeed: 100,
 		backSpeed: 60,
 		loop:true
